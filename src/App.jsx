e5045121d0e8cc143766ef04f7d3fd77c598deb8
rhmti01 <rahmati.business@gmail.com>
2025-01-01 00:11:30 +0330

    update structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' text-5xl font-bold text-cyan-50 ' >Vite + React + Tailwind</h1>
+    </>
+  )
+}
+
+export default App
